8af2abf94ff8e62e97e636c016cbce18e2f0770a
duong <duonghien491@gmail.com>
2024-10-10 01:38:52 +0700

    add_FragmentDiscover, Library, Search, MiniPlayer and add menu_bottom

---
+++ app/src/main/java/com/example/soundme/fragments/FragmentLibrary.java
@@ -0,0 +1,49 @@
+package com.example.soundme.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.soundme.R;
+import com.example.soundme.activities.PlaylistActivity;
+
+public class FragmentLibrary extends Fragment {
+    private ConstraintLayout liked_songs_frame;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_library, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addControl(view);
+        addEvent();
+    }
+
+    private void addControl(View view) {
+        liked_songs_frame = view.findViewById(R.id.liked_songs_frame);
+    }
+
+    private void addEvent() {
+        liked_songs_frame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PlaylistActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/soundme/fragments/FragmentSearch.java
@@ -0,0 +1,48 @@
+package com.example.soundme.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.example.soundme.R;
+import com.example.soundme.activities.InSearchActivity;
+
+public class FragmentSearch extends Fragment {
+    private FrameLayout search_frame;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        addControl(view);
+        addEvent();
+    }
+
+    private void addControl(View view) {
+        search_frame = view.findViewById(R.id.search_frame);
+    }
+
+    private void addEvent() {
+        search_frame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), InSearchActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
